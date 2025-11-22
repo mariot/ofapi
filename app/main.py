5cdbc36a530ba79752e6e890c46139bd5b6461f0
Mariot Tsitoara <mariot.tsitoara@filigran.io>
2025-11-22 10:46:02 +0100

    add reversinglabs

--- app/main.py
+++ app/main.py
@@ -4,12 +4,25 @@ import json
 from dataclasses import asdict
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+import fastapi
+from fastapi import FastAPI, File, Form, Query, UploadFile
 from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
 
 from app.api.feedly import BundleFactory
 from app.api.hunt_io import C2FeedFactory
 from app.api.proofpoint_tap import CampaignDetailFactory, CampaignFactory
+from app.api.reversinglabs import (
+    AnalysisResponseFactory,
+    DomainResponseFactory,
+    DownloadedFilesResponseFactory,
+    HashClassificationFactory,
+    ReportIntelligenceResponseFactory,
+    ReportResponseFactory,
+    ResolutionResponseFactory,
+    UploadDetailFactory,
+    UrlsResponseFactory,
+)
 from app.api.utils import remove_private_attributes
 
 app = FastAPI()
@@ -65,3 +78,152 @@ async def proofpoint_tap_campaign_details(campaign_id: str):
     Returns a sample response for Proofpoint TAP campaign details."""
     campaign = CampaignDetailFactory.create(id=campaign_id)
     return remove_private_attributes(asdict(campaign))
+
+
+@app.get(
+    "/reversinglabs-spectra-analyze/api/network-threat-intel/ip/{ip}/downloaded_files/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_ip_downloaded_files(ip: str):
+    """ReversingLabs Spectra Analyze Downloaded Files Endpoint"""
+    downloaded_files_response = DownloadedFilesResponseFactory(requested_ip=ip)
+    return asdict(downloaded_files_response)
+
+
+@app.get(
+    "/reversinglabs-spectra-analyze/api/network-threat-intel/ip/{ip}/report/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_ip_report(ip: str):
+    """ReversingLabs Spectra Analyze Report Endpoint"""
+    report_response = ReportResponseFactory(requested_ip=ip)
+    return remove_private_attributes(asdict(report_response))
+
+
+@app.get(
+    "/reversinglabs-spectra-analyze/api/network-threat-intel/ip/{ip}/resolutions/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_ip_resolutions(ip: str):
+    """ReversingLabs Spectra Analyze Resolutions Endpoint"""
+    resolution_response = ResolutionResponseFactory(requested_ip=ip)
+    return asdict(resolution_response)
+
+
+@app.get(
+    "/reversinglabs-spectra-analyze/api/network-threat-intel/ip/{ip}/urls/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_ip_urls(ip: str):
+    """ReversingLabs Spectra Analyze Urls Endpoint"""
+    urls_response = UrlsResponseFactory(requested_ip=ip)
+    return asdict(urls_response)
+
+
+@app.get(
+    "/reversinglabs-spectra-analyze/api/network-threat-intel/domain/{domain}/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_domain(domain: str):
+    """ReversingLabs Spectra Analyze Domain Endpoint"""
+    domain_response = DomainResponseFactory(requested_domain=domain)
+    return remove_private_attributes(asdict(domain_response))
+
+
+@app.get(
+    "/reversinglabs-spectra-analyze/api/network-threat-intel/url/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_url(url: str):
+    """ReversingLabs Spectra Analyze Domain Endpoint"""
+    analysis_response = AnalysisResponseFactory(requested_url=url)
+    return analysis_response.to_dict()
+
+
+@app.post(
+    "/reversinglabs-spectra-analyze/api/uploads/",
+    tags=["ReversingLabs Spectra Analyze"],
+    status_code=201,
+)
+async def reversinglabs_spectra_analyze_upload(
+    file: Annotated[UploadFile | None, File()] = None,
+    url: Annotated[str, Form()] = None,
+):
+    """ReversingLabs Spectra Analyze Upload Endpoint"""
+    filename = file.filename if file else url
+    return {
+        "code": 201,
+        "message": "Done.",
+        "detail": asdict(UploadDetailFactory(filename=filename)),
+    }
+
+
+@app.get(
+    "/reversinglabs-spectra-analyze/api/uploads/v2/url-samples/{task_id}/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_check_task(
+    task_id: str,  # noqa: ARG001
+):
+    """Check the processing status of a submitted URL."""
+    return {
+        "processing_status": "complete",
+        "message": "Processing complete.",
+        "report": remove_private_attributes(
+            asdict(ReportIntelligenceResponseFactory())
+        ),
+    }
+
+
+@app.get(
+    "/reversinglabs-spectra-analyze/api/samples/v3/{hash}/classification/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_hash_classification(hash: str):
+    """Check the processing status of a submitted URL."""
+    return remove_private_attributes(asdict(HashClassificationFactory(sha1=hash)))
+
+
+class Hash(BaseModel):
+    hash_values: list[str]
+
+
+@app.post(
+    "/reversinglabs-spectra-analyze/api/samples/status/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_samples_status(
+    hash: Hash, _: str = fastapi.Query(default="processed", alias="status")
+):
+    """Check the processing status of a sample."""
+    return {
+        "hash_type": "sha1",
+        "results": [
+            {"hash_value": value, "status": "processed"} for value in hash.hash_values
+        ],
+    }
+
+
+class HashDetail(BaseModel):
+    hash_values: list[str]
+    fields: list[str]
+    include_networkthreatintelligence: bool
+    skip_reanalysis: bool
+
+
+@app.post(
+    "/reversinglabs-spectra-analyze/api/samples/v2/list/details/",
+    tags=["ReversingLabs Spectra Analyze"],
+)
+async def reversinglabs_spectra_analyze_samples_list_details(
+    hash_detail: HashDetail,  # noqa: ARG001
+):
+    """Details of a sample."""
+    return {
+        "count": 1,
+        "next": None,
+        "previous": None,
+        "results": [
+            remove_private_attributes(asdict(ReportIntelligenceResponseFactory()))
+        ],
+    }
